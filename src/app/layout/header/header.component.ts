40b8f28e771abaf9f42a9c47b828a4f15097cae2
Marcelo Gomes <devmarceloimts@gmail.com>
2025-06-10 23:59:05 -0300

    Realizado versionamento de componente principal de construção de componente principal e regras de importação etc

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,13 @@
+// src/app/layout/header/header.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {}
+}
